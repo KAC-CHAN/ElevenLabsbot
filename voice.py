b8f42edb19228d965165d88f108e181cfa62013b
KAC CHAN <anicadebots@gmail.com>
2023-09-17 21:21:27 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -7,23 +7,33 @@ bot_token = "6593343536:AAHYW4ehuF535UHAS_KUYVTwBj26hqf8RQ8"
 
 api_key = "aaa8ac11336407eed87a0c569edfb359"
 
+# Initialize Pyrogram client
 client = pyrogram.Client("text2voice", api_id, api_hash, bot_token)
-eleven = ElevenClient(api_key) 
 
-@client.on_message()
-async def text2voice(client, message):
+# Initialize Eleven Labs client  
+eleven = ElevenClient(api_key)
 
-  text = message.text
+# Message handler
+@client.on_message()  
+async def handler(client, message):
 
+  # Get user text
+  text = message.text  
+
+  # Limit text length
   if len(text) > 150:
     text = text[:150] + "..."
 
   # Generate voice with Adam voice
   voice = eleven.generateVoice(text, voice="adam")   
 
+  # Trim if audio > 1 min
   if len(voice) > 60000:
     voice = voice[:60000]
 
+  # Reply audio to user
   await client.send_audio(chat_id=message.chat.id, audio=voice)
 
-client.run()
+# Run bot
+if __name__ == "__main__":
+  client.run()
